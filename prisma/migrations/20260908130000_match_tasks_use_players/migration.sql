7dbce530e37d3cfe93e2e090e456c8e65942f44d
ItsJustLiliana <marijnverheijden@gmail.com>
2026-09-08 16:54:05 +0200

    migration

--- prisma/migrations/20260908130000_match_tasks_use_players/migration.sql
+++ prisma/migrations/20260908130000_match_tasks_use_players/migration.sql
@@ -1,4 +1,4 @@
-ALTER TABLE "MatchTask" ADD COLUMN "playerId" TEXT;
+ALTER TABLE "MatchTask" ADD COLUMN IF NOT EXISTS "playerId" TEXT;
 
 UPDATE "MatchTask" AS task
 SET "playerId" = player.id
@@ -7,6 +7,6 @@ WHERE player."userId" = task."userId"
   AND player."teamId" = task."teamId";
 
 ALTER TABLE "MatchTask" ALTER COLUMN "userId" DROP NOT NULL;
-ALTER TABLE "MatchTask" DROP CONSTRAINT "MatchTask_matchId_taskType_userId_key";
-ALTER TABLE "MatchTask" ADD CONSTRAINT "MatchTask_matchId_taskType_playerId_key" UNIQUE ("matchId", "taskType", "playerId");
+DROP INDEX IF EXISTS "MatchTask_matchId_taskType_userId_key";
+CREATE UNIQUE INDEX "MatchTask_matchId_taskType_playerId_key" ON "MatchTask"("matchId", "taskType", "playerId");
 ALTER TABLE "MatchTask" ADD CONSTRAINT "MatchTask_playerId_fkey" FOREIGN KEY ("playerId") REFERENCES "Player"("id") ON DELETE CASCADE ON UPDATE CASCADE;
